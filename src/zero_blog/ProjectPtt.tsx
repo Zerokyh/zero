d0e5d5f420319442a59fc2713843fa602895fa34
Zerokyh <koreaguh@naver.com>
2024-09-24 22:21:04 +0900

    라우터 업데이트

--- src/zero_blog/ProjectPtt.tsx
+++ src/zero_blog/ProjectPtt.tsx
@@ -4,7 +4,7 @@ const ProjectPtt = () => {
       <div className="w-192 flex flex-col gap-14 ">
         <div>
           <span>[ 메인 화면 - 대시보드 ]</span>
-          <img src="./ptt/dashboard-1.png"></img>
+          <img src="./ptt/dashboard-1.png" alt=""></img>
         </div>
         <div className="flex flex-col">
           <span>
@@ -12,96 +12,96 @@ const ProjectPtt = () => {
             사이드바에 있는 &lt; 를 누르거나
           </span>
           <span> 빔처리 영역을 누르면 사이드바가 접힘 ]</span>
-          <img src="./ptt/dashboard-2.png"></img>
+          <img src="./ptt/dashboard-2.png" alt=""></img>
         </div>
         <div>
           <span>[ 사이드바 - 로그인 전 접힌 사이드바 ]</span>
-          <img src="./ptt/side-logout-1.png"></img>
+          <img src="./ptt/side-logout-1.png" alt=""></img>
         </div>
         <div>
           <span>[ 사이드바 - 로그인 전 펼친 사이드바 ]</span>
-          <img src="./ptt/side-logout-2.png"></img>
+          <img src="./ptt/side-logout-2.png" alt=""></img>
         </div>
         <div>
           <span>
             [ 로그인 페이지 - SpringBoot 백엔드 - 로그인 기능만 구현 ]
           </span>
-          <img src="./ptt/login-1.png"></img>
+          <img src="./ptt/login-1.png" alt=""></img>
         </div>
         <div>
           <span>[ 사이드바 - 로그인 후 접힌 사이드바 ]</span>
-          <img src="./ptt/side-login-1.png"></img>
+          <img src="./ptt/side-login-1.png" alt=""></img>
         </div>
         <div>
           <span>[ 사이드바 - 로그인 후 펼친 사이드바 ]</span>
-          <img src="./ptt/side-login-2.png"></img>
+          <img src="./ptt/side-login-2.png" alt=""></img>
         </div>
         <div>
           <span>[ 사이드바 - 달력과 오늘의 이벤트 ]</span>
-          <img src="./ptt/calendar-1.png"></img>
+          <img src="./ptt/calendar-1.png" alt=""></img>
         </div>
         <div>
           <span>[ 사이드바 - 이벤트가 있는 날 클릭시 오늘의 이벤트 탭 ]</span>
-          <img src="./ptt/calendar-2.png"></img>
+          <img src="./ptt/calendar-2.png" alt=""></img>
         </div>
         <div>
           <span>[ 사이드바 - 오른쪽 화살표를 눌러 주간 이벤트 탭 클릭 ]</span>
-          <img src="./ptt/calendar-3.png"></img>
+          <img src="./ptt/calendar-3.png" alt=""></img>
         </div>
         <div>
           <span>[ 사이드바 - 오른쪽 화살표를 눌러 월간 이벤트 탭 클릭 ]</span>
-          <img src="./ptt/calendar-4.png"></img>
+          <img src="./ptt/calendar-4.png" alt=""></img>
         </div>
         <div>
           <span>[ 사이드바 펼쳐서 설정 버튼 클릭 ]</span>
-          <img src="./ptt/side-setting.png"></img>
+          <img src="./ptt/side-setting.png" alt=""></img>
         </div>
         <div>
           <span>[ 테마 - 기본 - 페이지 ]</span>
-          <img src="./ptt/setting-theme-1.png"></img>
+          <img src="./ptt/setting-theme-1.png" alt=""></img>
         </div>
         <div>
           <span>[ 테마 - 기본 - 사이드바 ]</span>
-          <img src="./ptt/setting-theme-2.png"></img>
+          <img src="./ptt/setting-theme-2.png" alt=""></img>
         </div>
         <div>
           <span>[ 테마 - 선셋 - 페이지 ]</span>
-          <img src="./ptt/setting-theme-3.png"></img>
+          <img src="./ptt/setting-theme-3.png" alt=""></img>
         </div>
         <div>
           <span>[ 테마 - 선셋 - 사이드바 ]</span>
-          <img src="./ptt/setting-theme-4.png"></img>
+          <img src="./ptt/setting-theme-4.png" alt=""></img>
         </div>
         <div>
           <span>
             [ 사이드바 펼쳐서 설정 버튼 - 개인정보관리에서 편집 클릭 ]
           </span>
-          <img src="./ptt/myinfo-1.png"></img>
+          <img src="./ptt/myinfo-1.png" alt=""></img>
         </div>
         <div>
           <span>
             [ 개인정보관리 페이지 - 프로필 사진(외부 스토리지 - imgbb)과
             로그인한 계정 정보 DB에서 호출해서 출력 ]
           </span>
-          <img src="./ptt/myinfo-2.png"></img>
+          <img src="./ptt/myinfo-2.png" alt=""></img>
         </div>
         <div>
           <span>[ 프로필 이미지 변경 - 이미지에서 연필아이콘 클릭 ]</span>
-          <img src="./ptt/myinfo-3.png"></img>
+          <img src="./ptt/myinfo-3.png" alt=""></img>
         </div>
         <div>
           <span>[ 프로필 이미지 변경 - 원하는 이미지 클릭 ]</span>
-          <img src="./ptt/myinfo-4.png"></img>
+          <img src="./ptt/myinfo-4.png" alt=""></img>
         </div>
         <div>
           <span>
             [ 프로필 이미지 변경 - 이미지 불러올 때 까지 로딩표시 출력 ]
           </span>
-          <img src="./ptt/myinfo-5.png"></img>
+          <img src="./ptt/myinfo-5.png" alt=""></img>
         </div>
         <div>
           <span>[ 프로필 이미지 변경 - 업로드한 이미지 출력 ]</span>
-          <img src="./ptt/myinfo-6.png"></img>
+          <img src="./ptt/myinfo-6.png" alt=""></img>
         </div>
       </div>
     </div>
